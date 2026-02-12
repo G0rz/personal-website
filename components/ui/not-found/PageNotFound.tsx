c128f871917d4c46f9919a58b5fed3fdade5ae02
Leonardo Becerril <leonardo.becerril@homtmail.com>
2026-02-12 01:42:50 -0600

    feat:new resume file fix:update jobs bullet points and fix 404 page

--- components/ui/not-found/PageNotFound.tsx
+++ components/ui/not-found/PageNotFound.tsx
@@ -4,16 +4,13 @@ export const PageNotFound = () => {
     return (
         <div
             className="relative w-screen h-screen bg-black text-white font-custom tracking-tighter">
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col w-3/4">
-                <h3 className="w-full md:text-[4em] text-2xl font-bold font-mono">ERROR:404</h3>
-                <h2 className="w-full md:text-[7.25em] text-5xl md:-mt-4 -mt-1">NAVIGATION</h2>
-                <h4 className="place-self-end md:text-[2.5em]">You can (not)</h4>
-                <h1 className="w-full md:text-[4.60em] text-3xl">THE END OF</h1>
-                <h4 className="place-self-end md:text-[2.5em]">REDO.</h4>
-                <Link href="/"
-                      className="place-self-center mt-10 text-xl hover:underline hover:text-primary transition-colors">
-                    Return to Home
-                </Link>
+            <div
+                className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 transform scale-y-120">
+                <h1 className="w-full lg:text-[4.60em] text-3xl">THE END OF</h1>
+                <h2 className="w-full lg:text-[7.25em] text-5xl lg:-mt-4 -mt-1">NAVIGATION</h2>
+                <h3 className="w-full lg:text-[4em] text-2xl font-bold font-mono">ERROR:404</h3>
+                <h4 className="place-self-end lg:text-[2.5em]">You can(not)<Link href="/" className="hover:underline-offset-8 hover:underline">go back.</Link>
+                </h4>
             </div>
         </div>
     );
